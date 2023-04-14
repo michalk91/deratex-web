08a3d7d8f46bd4a44b75bd6a164cad3c3b7f8b50
Michal Kukla <michal.kukla91@gmail.com>
2023-04-14 11:37:38 +0200

    Height on mobile was returning wrong values. Now height value is stored in state so that the values ​​are correct

--- components/index/Reviews.js
+++ components/index/Reviews.js
@@ -9,6 +9,7 @@ import { useGlobalState } from "../../pages/_app";
 
 const Reviews = () => {
   const [iframeError, setIframeError] = useState(false);
+  const [height, setHeight] = useState(0);
 
   const ref = useRef();
 
@@ -20,16 +21,19 @@ const Reviews = () => {
     }
   });
 
-
-
+  useEffect(() => {
+    setHeight(ref?.current?.clientHeight);
+  });
 
   useEffect(() => {
     if (!fbSDKLoaded) return;
 
     FB.XFBML.parse(ref?.current, function () {
       //FB.XFBML.parse callback function invoked when elements are rendered
-      if (ref?.current?.clientHeight < 200) {setIframeError(true);}
-     //When the iframe is rendered we check if its height is greater than 200px. if so, we are sure that the iframe has been rendered correctly
+      if (height < 150) {
+        setIframeError(true);
+      }
+      //When the iframe is rendered we check if its height is greater than 150px. if so, we are sure that the iframe has been rendered correctly
     });
   }, [fbSDKLoaded]); // When the error "'<URL>' was refused to be displayed in the frame because 'X-Frame-Options' is set to 'deny'" the iframe will not load and the height will not increase
 
@@ -40,7 +44,7 @@ const Reviews = () => {
         <section className={styles.reviews} ref={ref}>
           <span className={classNames("title")}>Opinie o Nas</span>
           <div className="graySection">
-            <div className={classNames(styles.innerContent)} >
+            <div className={classNames(styles.innerContent)}>
               <RichCarousel
                 translateX="100%"
                 sliderRectanglesVisible={false}
