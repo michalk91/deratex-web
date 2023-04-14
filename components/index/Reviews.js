30ad10b8af1abb9f99f73fc4d20f5b5e6b8cce5e
Michal Kukla <michal.kukla91@gmail.com>
2023-04-15 01:21:16 +0200

    used setTimeout HACK instead of previous solutions

--- components/index/Reviews.js
+++ components/index/Reviews.js
@@ -20,18 +20,17 @@ const Reviews = () => {
     }
   });
 
-  useEffect(() => {
-    setHeight(ref?.current?.clientHeight);
-  });
 
   useEffect(() => {
     if (!fbSDKLoaded) return;
 
     FB.XFBML.parse(ref?.current, function () {
       //FB.XFBML.parse callback function invoked when elements are rendered
-      if (height < 150) {
-        setIframeError(true);
-      }
+      setTimeout(() => {         //HACK! Using setTimeout without second argument(time) to read height value after call stack is empty
+        if (ref.current.clientHeight < 150) {
+          setIframeError(true);
+        }
+      });
       //When the iframe is rendered we check if its height is greater than 150px. if so, we are sure that the iframe has been rendered correctly
     });
   }, [fbSDKLoaded]); // When the error "'<URL>' was refused to be displayed in the frame because 'X-Frame-Options' is set to 'deny'" the iframe will not load and the height will not increase
@@ -43,7 +42,7 @@ const Reviews = () => {
         <section className={styles.reviews} ref={ref}>
           <span className={classNames("title")}>Opinie o Nas</span>
           <div className="graySection">
-            <div className={classNames(styles.innerContent)} >
+            <div className={classNames(styles.innerContent)}>
               <RichCarousel
                 translateX="100%"
                 sliderRectanglesVisible={false}
