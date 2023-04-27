2dd62720c21ab46f6d2beada3760323ec0a3a621
Michal Kukla <michal.kukla91@gmail.com>
2023-04-27 11:07:38 +0200

    carousel height changed

--- components/index/Certificates.js
+++ components/index/Certificates.js
@@ -139,7 +139,7 @@ function Certificates() {
           // slideTime={3000}
           // width="70vw"
 
-          height= "220px"
+          height= "230px"
           virtualized
           // imagesForLightboxData={certificates} //add this when you want using virtualized and you added "withGallery" prop
           withGallery
