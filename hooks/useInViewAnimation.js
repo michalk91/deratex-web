82cb31183532341e338c01d9846a29a4295d268c
Michal Kukla <michal.kukla91@gmail.com>
2023-05-09 13:12:50 +0200

    mobile sensivity added

--- hooks/useInViewAnimation.js
+++ hooks/useInViewAnimation.js
@@ -1,7 +1,12 @@
 import { useCallback, useRef } from "react";
 import { useInViewport } from "react-in-viewport";
+import { isMobile } from "react-device-detect";
 
-function useInViewAnimation({ animateContainerRef, sensivity = "250" }) {
+function useInViewAnimation({
+  animateContainerRef,
+  sensivity = "250",
+  mobileSensivity = "150",
+}) {
   const animateRef = useRef(false);
 
   const onEnterViewport = useCallback(() => {
@@ -12,7 +17,7 @@ function useInViewAnimation({ animateContainerRef, sensivity = "250" }) {
 
   useInViewport(
     animateContainerRef,
-    { rootMargin: `-${sensivity}px` },
+    { rootMargin: !isMobile ? `-${sensivity}px` : `-${mobileSensivity}px` },
     {},
     { onEnterViewport }
   );
