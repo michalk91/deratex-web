ea59360715f002c44b5043eb3b2569641fa0d529
Michal Kukla <michal.kukla91@gmail.com>
2023-07-07 17:41:10 +0200

    next image used instead of css background-image

--- components/index/HelpForAllSlogan.js
+++ components/index/HelpForAllSlogan.js
@@ -2,18 +2,27 @@ import styles from "./helpForAllSlogan.module.css";
 import React from "react";
 import { memo } from "react";
 import classNames from "classnames";
+import Image from "next/image";
 
-function helpForAllSlogan(){
-    return(
-        <section className={styles.helpForAll}>
-        <div className={styles.helpForAllSloganTransparentBackground}>
-          <span className={classNames("innerContentWidth" ,styles.helpForAllSlogan)}>
-            W SZCZEGÓLNYCH PRZYPADKACH NIE ODMÓWIMY POMOCY NIKOMU NIEZALEŻNIE OD
-            POŁOŻENIA.
-          </span>
-        </div>
-      </section>
-    )
+function helpForAllSlogan() {
+  return (
+    <section className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/images/index/HelpForAllSlogan/backgroundImage.jpg"
+          alt="background image"
+          fill
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.transparentBackground}>
+        <span className={classNames("innerContentWidth", styles.text)}>
+          W SZCZEGÓLNYCH PRZYPADKACH NIE ODMÓWIMY POMOCY NIKOMU NIEZALEŻNIE OD
+          POŁOŻENIA.
+        </span>
+      </div>
+    </section>
+  );
 }
 
 export default memo(helpForAllSlogan);
