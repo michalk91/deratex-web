836602141356bb4060202c89febfca3aa173b8b3
Michal Kukla <michal.kukla91@gmail.com>
2023-07-07 18:06:47 +0200

    lang tag added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
-
-global.HTMLImageElement = typeof window === 'undefined' ? Object : window.HTMLImageElement
+global.HTMLImageElement =
+  typeof window === "undefined" ? Object : window.HTMLImageElement;
 
 module.exports = {
   reactStrictMode: true,
   images: {
-    deviceSizes: [280, 320, 360, 480 ,640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    deviceSizes: [
+      280, 320, 360, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840,
+    ],
     imageSizes: [8, 16, 32, 48, 64, 96, 128, 256, 384],
   },
 
-}
+  i18n: {
+    locales: ["pl"],
+    defaultLocale: "pl",
+  },
+};
