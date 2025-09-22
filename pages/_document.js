e037a9a101d635aef54119d6009ee735fba3a392
michalk91 <michal.kukla91@gmail.com>
2025-09-22 19:45:37 +0200

    Implement cookiebot

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,16 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pl">
-        <Head />
+        <Head>
+          <script
+            id="Cookiebot"
+            src="https://consent.cookiebot.com/uc.js"
+            data-cbid="22bb3d9e-666f-4b85-8a76-27cf0219503f"
+            data-blockingmode="auto"
+            type="text/javascript"
+            async
+          ></script>
+        </Head>
         <body>
           <Main />
 
